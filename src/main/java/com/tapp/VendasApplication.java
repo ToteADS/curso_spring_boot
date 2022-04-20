996cf7e40a095854c0041b6e2b1d8b78a204ec75
ToteADS <toteads@gmail.com>
2022-04-20 12:22:17 -0300

    salvando e recuperando clientes

--- src/main/java/com/tapp/VendasApplication.java
+++ src/main/java/com/tapp/VendasApplication.java
@@ -1,23 +1,32 @@
 package com.tapp;
 
+import com.tapp.domain.entitity.Cliente;
+import com.tapp.domain.repositories.Clientes;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
 @RestController
 public class VendasApplication {
 
-    @Autowired
-    @Qualifier("applicationName")
-    private String applicationName;
+    @Bean
+    public CommandLineRunner init(@Autowired Clientes clientes){
+        return  args -> {
+            clientes.salvar(new Cliente("Tote"));
+            clientes.salvar(new Cliente("Nalita"));
+            clientes.salvar(new Cliente("Ana Clara"));
 
-    @GetMapping("/hello")
-    public String helloWord(){
-        return applicationName;
+            List<Cliente> todosClientes =  clientes.obterTodos();
+            todosClientes.forEach(System.out::println);
+        };
     }
 
     public static void main(String[] args) {
