61debf5411f71288c52107d6313cdbc5e31fb293
ToteADS <toteads@gmail.com>
2022-04-20 11:05:51 -0300

    subindo projeto

---
+++ src/main/java/com/tapp/repository/ClientesRepository.java
@@ -0,0 +1,12 @@
+package com.tapp.repository;
+
+import com.tapp.model.Cliente;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ClientesRepository {
+
+    public void persistir(Cliente cliente) {
+
+    }
+}
