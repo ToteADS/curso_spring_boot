6478c81cd1dd4317730c150557ce95ade95012f7
ToteADS <toteads@gmail.com>
2022-04-22 14:03:04 -0300

    trabalhando com @Query

--- src/main/java/com/tapp/domain/repositories/Clientes.java
+++ src/main/java/com/tapp/domain/repositories/Clientes.java
@@ -1,13 +1,21 @@
 package com.tapp.domain.repositories;
 import com.tapp.domain.entitity.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 
 public interface Clientes extends JpaRepository<Cliente,Integer> {
 
-    List<Cliente> findByNomeContaining(String nome);
+    @Query(value = " select * from cliente c where c.nome like '%:nome%' ", nativeQuery = true)
+    List<Cliente> findByNomeContaining(@Param("nome") String nome);
+
+    @Query("delete from Cliente c where c.nome = :nome")
+    @Modifying
+    void deleteByNome(String nome);
 
     boolean existsByNome(String nome);
 }
