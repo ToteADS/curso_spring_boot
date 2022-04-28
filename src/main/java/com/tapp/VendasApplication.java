91126db68d559d2dae7687ac0b512d66d4ce2e37
ToteADS <toteads@gmail.com>
2022-04-28 11:09:46 -0300

    Requisição GET com parametros e Response Entities

--- src/main/java/com/tapp/VendasApplication.java
+++ src/main/java/com/tapp/VendasApplication.java
@@ -1,12 +1,25 @@
 package com.tapp;
 
+import com.tapp.domain.entitities.Cliente;
+import com.tapp.domain.repositories.Clientes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class VendasApplication {
+
+    @Bean
+    public CommandLineRunner commandLineRunner(@Autowired Clientes clientes){
+        return args -> {
+            Cliente c = new Cliente(null, "Tote");
+            clientes.save(c);
+        };
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
