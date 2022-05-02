1fa41747e71b8021965a60d7e1e957470edf5bf0
ToteADS <toteads@gmail.com>
2022-05-02 13:49:43 -0300

    validacao produto

--- src/main/java/com/tapp/domain/entitities/Produto.java
+++ src/main/java/com/tapp/domain/entitities/Produto.java
@@ -3,6 +3,8 @@ package com.tapp.domain.entitities;
 import lombok.*;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 
 @Data
@@ -15,8 +17,12 @@ public class Produto {
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id")
     private Integer id;
+
+    @NotEmpty(message = "Campo descricao é Obrigatorio")
     @Column(name = "descricao")
     private String descricao;
+
+    @NotNull(message = "Campo Preco é Obrigatorio")
     @Column(name = "preco_unitario")
     private BigDecimal preco;
 
