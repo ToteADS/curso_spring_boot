295c7686649675e4e351526e8476fc64681b829f
ToteADS <toteads@gmail.com>
2022-05-02 14:06:05 -0300

    Validando Pedido e criando annotation customizada

--- src/main/java/com/tapp/rest/dto/PedidoDTO.java
+++ src/main/java/com/tapp/rest/dto/PedidoDTO.java
@@ -1,9 +1,11 @@
 package com.tapp.rest.dto;
 
+import com.tapp.validation.NotEmptyList;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import java.util.List;
 
@@ -11,7 +13,10 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 public class PedidoDTO {
+    @NotNull(message = "Informe o codigo do cliente")
     private Integer cliente;
+    @NotNull(message = "Campo Total do pedido é obrigatorio")
     private BigDecimal total;
+    @NotEmptyList(message = "Pedido não pode ser realizado sem itens")
     private List<ItemPedidoDTO> items;
 }
